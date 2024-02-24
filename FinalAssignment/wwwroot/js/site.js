f293f4ffc96ed426c1612bfcca4218f5e67d290c
Bendrick01 <131765016+Bendrick01@users.noreply.github.com>
2024-02-24 10:52:59 +0000

    Add project files.

---
+++ FinalAssignment/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// JavaScript Animation Example (Rotate the heading)
+document.addEventListener('DOMContentLoaded', function () {
+    const heading = document.querySelector('h1');
+
+    function rotateHeading() {
+        heading.style.transition = 'transform 1s';
+        heading.style.transform = 'rotate(360deg)';
+    }
+
+    // Call the rotateHeading function after a delay (e.g., 2 seconds)
+    setTimeout(rotateHeading, 2000);
+});
